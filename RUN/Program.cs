bb494df4498aaad519bf28633be858cf45d05dc5
saabyer <salarclydexavier@gmail.com>
2023-05-09 14:49:28 +0800

    added RUN project

---
+++ RUN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Resources;
+using NAVI;
+using Newtonsoft.Json;
+using RUN.Properties;
+
+Console.WriteLine("Hello, World!");
+string filePath = AppDomain.CurrentDomain.BaseDirectory;
+string fileName = "shabs.sc";
+
+
+Image i = Resources.grass;
+
+List<Scene> scenes = new List<Scene>();
+scenes.Add(new Scene(i, "DCST"));
+
+
+string contents = JsonConvert.SerializeObject(scenes);
+System.IO.File.WriteAllText(Path.Combine(filePath, fileName), contents);
