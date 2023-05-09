8aae15576675d92ba6210313fc7c1785fa4b9f74
saabyer <salarclydexavier@gmail.com>
2023-05-09 13:51:27 +0800

    Add project files.

---
+++ NAVI/DataPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace NAVI
+{
+    public class DataPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string Name { get; set; }
+
+        public DataPoint(double x, double y, string name)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Name = name;
+        }
+    }
+}
---
+++ NAVI/Scene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+//using System.Net.Mime.MediaTypeNames;
+
+
+namespace NAVI
+{
+    public class Scene
+    {
+        public string Name;
+
+        public Image SceneImage { get; set; }
+        public byte[] ImageData;
+
+        public Scene(Image scene_image, string name)
+        {
+            this.Name = name;
+            this.SceneImage = scene_image;
+            
+        }
+
+    }
+}
